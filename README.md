ae1cc21ba1b79f99ce6ba655ddcd938e62bd0e99
Sandro <31536101+sandromund@users.noreply.github.com>
2023-12-19 21:29:26 +0100

    Rework

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# abc_simulation
-Implementation and visualisation of the artificial bee colony (ABC) optimization algorithm.
+# ABC Optimization Algorithm
+
+My implementation and visualisation of the [artificial bee colony (ABC)](https://en.wikipedia.org/wiki/Artificial_bee_colony_algorithm) optimization algorithm.
+
+I left out the evaluation part by adding more bees and was surprised that the optimizations still worked well. 
+
+
+<p align="center">
+    <img src="abc.gif"  width="60%" height="30%">
+</p>
+
+To illustrate this, we search for the four local minima of the
+def [Himmelblau function](https://en.wikipedia.org/wiki/Himmelblau%27s_function).
+
+````python
+f(x, y) == (x ** 2 + y - 11) ** 2 + (x + y ** 2 - 7) ** 2
+````
+
+<p align="center">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Himmelblau_function.svg/1280px-Himmelblau_function.svg.png"  width="60%" height="30%">
+</p>
+
+
+# Create a .gif from frames
+Useful code form [stack overflow](https://stackoverflow.com/questions/753190/programmatically-generate-video-or-animated-gif-in-python).
+Set ``save_frames`` to true in `main.py` and then create a gif from the png images. 
+````python
+import glob
+import contextlib
+from PIL import Image
+
+fp_in = "*.png"
+fp_out = "abc.gif"
+
+with contextlib.ExitStack() as stack:
+    frames = (stack.enter_context(Image.open(f)) for f in sorted(glob.glob(fp_in)))
+    img = next(frames)
+    img.save(fp=fp_out, format='GIF', append_images=frames, save_all=True, duration=200, loop=0)
+
+````
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import pygame
-import sys
-import numpy as np
 import random
+import sys
+
+import numpy as np
+import pygame
 
 window_size = 1000
 pygame.init()
 clock = pygame.time.Clock()
 fps = 30
 
+
+# TODO Refactoring and comments
 
 def f(x, y) -> float:
     # Himmelblau's function
@@ -40,10 +43,10 @@ screen = pygame.display.set_mode((window_size, window_size))
 pygame.display.set_caption("ABC Sim")
 bg = create_map(window_size)
 
-n = 10000
+n = 100
 # The ABC generates a randomly distributed initial population of SN solutions (food sources),
-# where SN denotes the swarm size
-solutions = [(random.randint(0, window_size-1), random.randint(0, window_size-1)) for _ in range(n)]
+# where n denotes the swarm size
+solutions = [(random.randint(0, window_size - 1), random.randint(0, window_size - 1)) for _ in range(n)]
 dimension = 2  # x and y
 
 
@@ -54,8 +57,8 @@ def generate_new_candidate_solution(k):
         j = random.randint(0, n - 1)
         while j == i:
             j = random.randint(0, n - 1)
-        l = [solutions[k][i], solutions[j][i]]
-        candidate_solution.append(round(solutions[k][i] + o * (max(l) - min(l))) % 1000)
+        solutions_l = [solutions[k][i], solutions[j][i]]
+        candidate_solution.append(round(solutions[k][i] + o * (max(solutions_l) - min(solutions_l))) % 1000)
     return candidate_solution
 
 
@@ -63,6 +66,9 @@ def fitness(solution):
     x, y = solution
     return bg[x][y][1]
 
+
+save_frames = False
+frame_count = 0
 
 running = True
 while running:
@@ -76,18 +82,22 @@ while running:
         if fitness(c) < fitness(solutions[k]):
             solutions[k] = c
 
+    # TODO Bees evaluates its nectar amount and dances in the hive
 
-
-    # #then evaluates its nectar amount and dances in the hive
-
-    # pygame.display.flip()
     pygame.surfarray.blit_array(screen, bg)
-
     for k in range(n):
         pygame.draw.circle(screen, (255, 255, 255), solutions[k], 3)
-
     pygame.display.update()
+
+    if save_frames:
+        frame_count += 1
+        filename = "screen_%04d.png" % (frame_count)
+        pygame.image.save(screen, filename)
+
     clock.tick(fps)
+
+    if frame_count > fps * 10:
+        break
 
 pygame.quit()
 sys.exit()
